2d69dc043720421e633387174f7eff254865b556
Gitanjit Medhi <gitanjit6@gmail.com>
2021-05-17 22:58:40 +0530

    model changes 1

--- contactus/models.py
+++ contactus/models.py
@@ -1,28 +1,29 @@
 from django.db import models
-
+#from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 #Head of the Centre
 
-class head_of_center(models.Model):
+class Head_of_center(models.Model):
     photo_head = models.ImageField(upload_to='img')
     text_head = models.CharField(max_length=500)
+    #points =models.ArrayField(models.CharField(max_length=2000))
     points_text = models.CharField(max_length=6000)
 
 #Faculty members/coordinators
 
-class faculty_members(models.Model):
+class Faculty_members(models.Model):
     photo_head = models.ImageField(upload_to='img')
     text_head = models.CharField(max_length=500)
     former_text = models.CharField(max_length=2000)
 
-class department_reps(models.Model):    
+class Department_reps(models.Model):    
     department = models.CharField(max_length=200)
     points_text = models.CharField(max_length=6000)
 
 #ccd-office
 
-class ccd_office(models.Model):
+class Ccd_office(models.Model):
     title = models.CharField(max_length=200)
     points_text = models.CharField(max_length=5000)
     photo_head = models.ImageField(upload_to='img')
@@ -30,12 +31,15 @@ class ccd_office(models.Model):
 
 #Student Coordinators
 
-class placement_coordinators(models.Model):
-    contact_det = models.CharField(max_length=500)
+class Contacts(models.Model):
+    topic = models.CharField(max_length=100)
+    email = models.URLField(max_length=100)
+
+class Placement_coordinators(models.Model):
     photo_coord = models.ImageField(upload_to='img')
     text_coord = models.CharField(max_length=500)
 
-class intern_coordinators(models.Model):
+class Intern_coordinators(models.Model):
     text_coord = models.CharField(max_length=500)
 
 
